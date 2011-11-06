5c10492cc2574abfaeb2e70c3f78ef69c9abb017
Caroline Leung <carolineleung@Caroline-Leungs-MacBook-Pro.local>
2011-11-06 10:07:13 -0500

    Carol: Added Card and Carddeck model

---
+++ src/com/caroldev/flashcard/model/Card.java
@@ -0,0 +1,45 @@
+package com.caroldev.flashcard.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class Card {
+
+	private String question;
+	private String answer;
+	private List<String> tags;
+	private Map<String, String> fields;
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+	public Map<String, String> getFields() {
+		return fields;
+	}
+
+	public void setFields(Map<String, String> fields) {
+		this.fields = fields;
+	}
+
+}
---
+++ src/com/caroldev/flashcard/model/CardDeck.java
@@ -0,0 +1,80 @@
+package com.caroldev.flashcard.model;
+
+import java.util.List;
+
+public class CardDeck {
+
+	private String name;
+	private String id;
+	private String description;
+	private List<String> tags;
+	private long lastModifiedDate;
+	private String lastModifiedBy;
+	private String createdBy;
+	private List<Card> cards;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+	public long getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(long lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(String lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	public List<Card> getCards() {
+		return cards;
+	}
+
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+}
